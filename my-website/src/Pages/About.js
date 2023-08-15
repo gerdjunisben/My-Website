606a58867c004598e2ed41512d7ec2577fd0144e
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2023-08-14 23:06:14 -0400

    Base site built and uploaded at Benezuela.net

--- my-website/src/Pages/About.js
+++ my-website/src/Pages/About.js
@@ -3,6 +3,11 @@ function About(){
     
     return <div>
         <h1>About Page</h1>
+        <t class="aboutText">Hi, I'm Benjamin Gerdjunis and this is my website. <br/>
+        This is a fairly simple site to show that I can make one as well as display a few other projects.<br/>
+        Anyways heres my <a href="https://github.com/gerdjunisben">github</a>, sorry, a lot of it is mostly private so that my previous professors don't have to rewrite everything to avoid cheating.<br/>
+        Here's my <a href = "mailto: benjamingerdjunis@gmail.com">email</a> if you have to reach me.<br/>
+        Have a lovely rest of your day or night :)</t>
     </div>
 }
 
--- my-website/src/Pages/Home.js
+++ my-website/src/Pages/Home.js
@@ -4,6 +4,7 @@ function Home(){
     
     return <div>
         <h1>Welcome to Benezuela.net</h1>
+        <h2>I am in no way associated with Venezuela but I am certainly Ben</h2>
     </div>
 }
 
